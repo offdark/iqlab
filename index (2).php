112a975db194a0f4fd6ee54a965a5631c7f466eb
Vladimir <vtoroniy@gmail.com>
2014-05-06 19:47:47 +0300

    almost done

--- index (2).php
+++ index (2).php
@@ -1,93 +1,102 @@
 <?php
+/*
+$num_files = count(glob('user_images/*'));
 
 
-$link = 'user_images/u0/mike/';
 
 $uploadfile = $link . basename($_FILES['userfile']['name']);
 
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Файл корректен и был успешно загружен.\n";
+    echo "good.\n";
 } else {
-    echo "Возможная атака с помощью файловой загрузки!\n";
+    echo "not good!\n";
 }
 
- print_r($_FILES);
 $user = 'mike';
+ **/
 
 
 
+function save_image($user_id=0){
 
-function create_folders( $user_id, $flag ){
-
-$u_id        = 0;
-$folder_id   = 0;
-
-$user_images = 'user_images';
-$u           = $user_images.'/u'.$u_id;
-$id_user     = $u.'/'.$user_id;
-$folder_id   = $id_user. '/' .$folder_id;
+    $user_images = 'user_images';
 
 
-    switch ($flag) {
-        case 0:
-        
-             mkdir($user_images);
-          
-        case 1:
-        
-             mkdir($u);
-            
-        case 2:
-        
-            mkdir($id_user);
-            
-        case 3:
-        
-            mkdir($folder_id);
-            break;
-        default:
-            echo "x не равен 0, 1 или 2";
+    if ( is_dir($user_images) ) { //checking if user_images
+
+        for( $i = 0; $i <= 3; $i++ ){ //START FIRST FOR
+            $u = $user_images. '/u'. $i; // creating folder U0
+
+            if( is_dir($u) ){ //cheking if folder Ui exists
+
+                if( $num_files = count(glob($u. '/*')) < 3 ){
+
+                        if( is_dir($u.'/'.$user_id )){
+
+                          x:  for( $j = 1; $j <= 3; $j++ ){ //START SECOND FOR
+                                $folder = $u. '/' .$user_id. '/' .$j; // NAME FROM 0 TO 255
+
+                                if( is_dir( $folder) ){
+
+                                    if( $num_files_last = count(glob($folder. '/*')) < 3 ){
+
+                                        echo "fuck";
+                                        return;
+
+                                    }
+                                } else{
+                                    mkdir( $folder );
+                                    $file_name = $folder. '/index.html';
+                                    $open_file = fopen($file_name, 'w');
+                                    $data = '<h1> fuck you </h1>';
+                                    fwrite( $open_file, $data );
+                                    fclose( $open_file );
+                                    --$j;
+
+                                }
+                            } //END SECOND FOR
+                        } else {
+                            mkdir( $u. '/'. $user_id );
+                            $file_name = $u. '/' .$user_id. '/index.html';
+                            $open_file = fopen($file_name, 'w');
+                            $data = '<h1> fuck you </h1>';
+                            fwrite( $open_file, $data );
+                            fclose( $open_file );
+                                goto x;
+
+                        }
+
+                }
+
+            } else {
+                mkdir( $u );
+                $file_name = $u. '/index.html';
+                $open_file = fopen($file_name, 'w');
+                $data = '<h1> fuck you </h1>';
+                fwrite( $open_file, $data );
+                fclose( $open_file );
+                   --$i;
+            }
+        }//END FIRST FOR
+
+
+    } else {
+        mkdir( $user_images );
+        $file_name = $user_images. '/index.html';
+        $open_file = fopen($file_name, 'w');
+        $data = '<h1> fuck you </h1>';
+        fwrite( $open_file, $data );
+        fclose( $open_file );
+            return save_image();
+
     }
 
 }
+$stas = 'iii';
+save_image($stas);
 
-if ( is_dir($user_images) ) { //checking if folder exist
 
-    echo "The file $user_images exists";
-
-    if( is_dir($u) ){ // U0 folder
-
-        echo "folder UO exists";
-
-        if( is_dir($id_user) ){ // ID_USER folder
-
-            echo $id_user;
-
-            //todo algoritm
-
-        }
-        else{
-            mkdir($id_user);
-        }
-    }
-    else{
-       mkdir($u);
-    }
-
-} else {
-    mkdir($user_images);
-}
-$num_files = count(glob('user_images/u0/*'));
-    
-    if( $num_files >= 4 ){
-            
-            // mkdir('user_images/u1');
-        
-        //todo create function with saving inf into
-    }
-
-echo $num_files;
 ?>
 <!DOCTYPE html>
 <html>
@@ -98,12 +107,12 @@ echo $num_files;
 
 <body>
 
-<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
+
 <form enctype="multipart/form-data" action="index.php" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+    <!--  MAX_FILE_SIZE -->
     <input type="hidden" name="MAX_FILE_SIZE" value="300000000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
-    Отправить этот файл: <input name="userfile" type="file" />
+
+    Select a file: <input name="userfile" type="file" />
     <input type="submit" value="Send File" />
 </form>
 </body>
