44e94e7fec4620b4e9d47e93677fa94c7dd86701
offdark <vtoroniy@gmail.com>
2014-05-06 15:34:23 +0300

    create function for checking files

--- index (2).php
+++ index (2).php
@@ -1,14 +1,54 @@
 <?php
+
+
+$link = 'user_images/u0/mike/';
+
+$uploadfile = $link . basename($_FILES['userfile']['name']);
+
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Файл корректен и был успешно загружен.\n";
+} else {
+    echo "Возможная атака с помощью файловой загрузки!\n";
+}
+
+ print_r($_FILES);
 $user = 'mike';
 
 
+
+
+function create_folders( $user_id, $flag ){
+
+$u_id        = 0;
+$folder_id   = 0;
+
 $user_images = 'user_images';
-$u = $user_images.'/u0';
-$id_user = $u.'/'.$user;
+$u           = $user_images.'/u'.$u_id;
+$id_user     = $u.'/'.$user_id;
+$folder_id   = $id_user. '/' .$folder_id;
 
-function create_folders(){
 
-
+    switch ($flag) {
+        case 0:
+        
+             mkdir($user_images);
+          
+        case 1:
+        
+             mkdir($u);
+            
+        case 2:
+        
+            mkdir($id_user);
+            
+        case 3:
+        
+            mkdir($folder_id);
+            break;
+        default:
+            echo "x не равен 0, 1 или 2";
+    }
 
 }
 
@@ -38,8 +78,16 @@ if ( is_dir($user_images) ) { //checking if folder exist
 } else {
     mkdir($user_images);
 }
+$num_files = count(glob('user_images/u0/*'));
+    
+    if( $num_files >= 4 ){
+            
+            // mkdir('user_images/u1');
+        
+        //todo create function with saving inf into
+    }
 
-
+echo $num_files;
 ?>
 <!DOCTYPE html>
 <html>
@@ -50,55 +98,13 @@ if ( is_dir($user_images) ) { //checking if folder exist
 
 <body>
 
-<form method="POST" action="index.php">
-
-    Add to the end of list: <input name="text"  type="text" required>
-    <input type="submit"  name="add" value="Add" />
-    <input name="reset"  type="reset" value="Clear"/>
-
+<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
+<form enctype="multipart/form-data" action="index.php" method="POST">
+    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="300000000" />
+    <!-- Название элемента input определяет имя в массиве $_FILES -->
+    Отправить этот файл: <input name="userfile" type="file" />
+    <input type="submit" value="Send File" />
 </form>
-<br />
-Add element after any position listed:
-<form method="POST" action="index.php">
-
-    ID: <input name="looking_id"  type="text" required>     Text: <input name="text"  type="text" required>
-    <input type="submit"  name="add_position" value="Add" />
-    <input name="reset"  type="reset" value="Clear"/>
-
-</form>
-<br />
-
-<table  border="0" width="600" > <!-- START of Second table -->
-
-    <tr bgcolor="#9acd32">
-
-        <th>ID</th>
-        <th>Text</th>
-        <th colspan="2">Edit</th>
-
-    </tr>
-
-
-
-            <form method="POST" action="index.php">
-
-                <tr>
-
-                    <td  align="center"></td>
-                    <td  align="center"> </td>
-                    <td  align="center">
-                        <input type="submit"  name="edit" value="Edit" />
-                    </td>
-                    <td  align="center">
-                        <input type="submit"  name="delete" value="Delete" onclick="return deleletconfig()" />
-                        <input type="hidden" name="id" value="" />
-                    </td>
-
-                </tr>
-
-            </form>
-
-
-</table> <!-- END of Second table -->
 </body>
 </html>
