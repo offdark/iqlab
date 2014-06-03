22e85282f4d02fe869f2d45d667cb0b0919d8530
offdark <vtoroniy@gmail.com>
2014-06-03 15:58:10 +0300

    add new password generator

--- chess/newPass.php
+++ chess/newPass.php
@@ -5,3 +5,36 @@
  * Date: 5/29/14
  * Time: 6:30 PM
  */ 
+ 
+ include 'includes/functions.php';
+ include 'html/header.inc';
+
+    if( $_SESSION['fileName'] == 'secretQ.php' ){
+        $new_password = $_SESSION['new_password'];
+        session_unset();
+    }
+    else{   session_unset();  header( 'Location: login.php' );  }
+ ?>
+ 
+   <div class="container-content">
+
+            <?php echo "your new password is: ". $new_password;
+                    echo "<br> now you can log in using your new password =) "; ?>
+       </div>
+
+        <div class="container-footer">
+
+            footer
+
+        </div>
+
+    </div>
+
+
+
+
+
+</body>
+</html>
+
+ 
