481e9b338336192a02a4c14147759904abf3e566
offdark <vtoroniy@gmail.com>
2014-06-08 16:14:03 +0300

    bug fix

--- chess/includes/Admin.class.php
+++ chess/includes/Admin.class.php
@@ -7,15 +7,15 @@
  */
 
 class Admin extends User {
-
+    
     public $data = array();
 
     public function allUsers(  ){
-
+     
         try{
             $STH = MYSQLDb::select( '*', $this->table_name );
             $STH->setFetchMode( PDO::FETCH_ASSOC ); // FetchMODE Array
-
+            
             foreach( $STH->fetchAll() as $key => $value){
 
                 $this->data[$key] = $value;
