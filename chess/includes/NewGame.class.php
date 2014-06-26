5dda6504c301776e15400daa00566c9d97b930d8
Vladimir <vtoroniy@gmail.com>
2014-06-26 19:43:06 +0300

    bug fix

--- chess/includes/NewGame.class.php
+++ chess/includes/NewGame.class.php
@@ -118,7 +118,7 @@ class NewGame  {
                 $table .= "<tr class='border_top'>  <td bgcolor='#FFFFFF' colspan='8'> Created: ". $values_arr['created'] ."</td></tr>";
                 $table .= "<td bgcolor='#FFFFFF'colspan='8'> Author: ". $values_arr['author_user_login'] ."</td>";
                 $table .= "<tr > <td bgcolor='#FFFFFF' colspan='8'>My color: ". $color = ( $values_arr['author_user_login'] != $_SESSION['login'] ) ? 'black' : 'white' ."</td></tr>";
-                $table .= "<tr > <td bgcolor='#FFFFFF' colspan='8'>". $nextMove = ( $values_arr['next_go'] == 'whiteFirstMove' && $color != 'black' ) ? " <a href='". URL ."?mod=". htmlspecialchars($values_arr['id']) ."&action=play'> Play</a></td></tr>" : "Waite for a partner first move </td></tr>" ;
+                $table .= "<tr > <td bgcolor='#FFFFFF' colspan='8'>". $nextMove = ( $values_arr['next_go'] != 'whiteFirstMove' && $color == 'black' ) ? " <a href='". URL ."?mod=". htmlspecialchars($values_arr['id']) ."&action=play'> Play</a></td></tr>" : "Waite for a partner first move </td></tr>" ;
              //   echo $values_arr['next_go'];
                   //     $table .= "</table>";
             
